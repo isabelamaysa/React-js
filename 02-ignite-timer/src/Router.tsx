d1202434e33b803f14281d1d7e7c076ea942a136
Isabela Maysa Souza Correa <isabelamaysa2002@gmail.com>
2023-11-21 08:20:29 -0300

    inicio

---
+++ 02-ignite-timer/src/Router.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        {/* Para que seja aplicado em todas as rotas(paginas) */}
+        <Route path="/" element={<Home />} />
+        {/* path = é para escolher qual o caminho para aquela rota(pagina), elemento = para onde aquele caminho digitado ira dar */}
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  )
+}
