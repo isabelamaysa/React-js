5196e6058f168bcfa18b167df5faca16859b19bd
Isabela Maysa Souza Correa <isabelamaysa2002@gmail.com>
2023-11-22 08:26:35 -0300

    att

--- 02-ignite-timer/src/layouts/DefaultLayout/styles.ts
+++ 02-ignite-timer/src/layouts/DefaultLayout/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   max-width: 1020px;
-  height: calc(100vh - 10rem);
+  min-height: calc(100vh - 10rem);
   margin: 5rem auto;
   padding: 2.5rem;
 
